99f7df216a48ecf3559410d5428a3833cc73dba3
Carlos Zapata <czapata.arg@gmail.com>
2022-07-31 15:08:42 -0300

    inicio del proyecto

---
+++ Piramide invertida/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Piramide_invertida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
